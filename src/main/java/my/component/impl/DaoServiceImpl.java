ebc5e1c7d9bd7ec111d375d0b877287fde802617
Olexandr Shaposhnykov <olexandr_shaposhnykov@epam.com>
2018-02-24 22:18:10 +0200

    Changed classes

--- src/main/java/my/component/impl/DaoServiceImpl.java
+++ src/main/java/my/component/impl/DaoServiceImpl.java
@@ -10,34 +10,57 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import javax.transaction.UserTransaction;
 
-import static my.hibernate.config.HibernateConfig.createSessionFactory;
-import static my.hibernate.config.HibernateConfig.getUserTransaction;
+//import static my.hibernate.config.HibernateConfig.createSessionFactory;
+//import static my.hibernate.config.HibernateConfig.getUserTransaction;
 
 //@Service("dao")
 public class DaoServiceImpl implements DaoService {
-    @Override
-    public SessionFactory getSessionFactory() {
-        return createSessionFactory();
-    }
+
+
 
     @Override
     public UserTransaction getuserTransaction() {
-        return getUserTransaction();
+        return null;
     }
 
     @Override
     public void rollback() {
-        try {
-            rollback();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
 
     }
 
     @Override
     public EntityManager createEntityManager() {
-        EntityManagerFactory factory = Persistence.createEntityManagerFactory("123456QWER");
-        return factory.createEntityManager();
+        return null;
     }
+
+    @Override
+    public SessionFactory getSessionFactory() {
+        return null;
+    }
+
+//    @Override
+//    public SessionFactory getSessionFactory() {
+//        return createSessionFactory();
+//    }
+//
+//    @Override
+//    public UserTransaction getuserTransaction() {
+//        return getUserTransaction();
+//    }
+//
+//    @Override
+//    public void rollback() {
+//        try {
+//            rollback();
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//
+//    }
+//
+//    @Override
+//    public EntityManager createEntityManager() {
+//        EntityManagerFactory factory = Persistence.createEntityManagerFactory("123456QWER");
+//        return factory.createEntityManager();
+//    }
 }
--- src/main/test/my/hibernate/CheckConnection.java
+++ src/main/test/my/hibernate/CheckConnection.java
@@ -12,43 +12,43 @@ import java.util.List;
 
 public class CheckConnection {
 
-    @Test
-    public void shouldConnectionAlive() throws Exception {
-
-        UserTransaction tx=HibernateConfig.getUserTransaction();
-        tx.begin();
-        SessionFactory sessionFactory = HibernateConfig.createSessionFactory();
-        //EntityManagerFactory entityManagerFactory =new EntityManagerFactoryImpl(sessionFactory);
-        Session session = sessionFactory.getCurrentSession();
-        Assert.assertTrue(session.isConnected());
-        tx.rollback();
-    }
-
-    @Test
-    public void shouldSetMessage() throws Exception {
-
-
-        try {
-            UserTransaction tx=HibernateConfig.getUserTransaction();
-        tx.begin();
-        SessionFactory sessionFactory = HibernateConfig.createSessionFactory();
-        Session session = sessionFactory.getCurrentSession();
-        Foreignwords foreignwords = new Foreignwords();
-        foreignwords.setForeignWord("hello");
-        session.persist(foreignwords);
+//    @Test
+//    public void shouldConnectionAlive() throws Exception {
+//
+//        UserTransaction tx=HibernateConfig.getUserTransaction();
+//        tx.begin();
+//        SessionFactory sessionFactory = HibernateConfig.createSessionFactory();
+//        //EntityManagerFactory entityManagerFactory =new EntityManagerFactoryImpl(sessionFactory);
+//        Session session = sessionFactory.getCurrentSession();
+//        Assert.assertTrue(session.isConnected());
+//        tx.rollback();
+//    }
+//
+//    @Test
+//    public void shouldSetMessage() throws Exception {
+//
+//
+//        try {
+//            UserTransaction tx=HibernateConfig.getUserTransaction();
+//        tx.begin();
+//        SessionFactory sessionFactory = HibernateConfig.createSessionFactory();
+//        Session session = sessionFactory.getCurrentSession();
+//        Foreignwords foreignwords = new Foreignwords();
+//        foreignwords.setForeignWord("hello");
+//        session.persist(foreignwords);
+////        tx.commit();
+//        List<Foreignwords> list=session.createCriteria(Foreignwords.class).list();
+//        Assert.assertEquals(list.size(),1);
+//        list.get(0).setForeignWord("Hello world");
 //        tx.commit();
-        List<Foreignwords> list=session.createCriteria(Foreignwords.class).list();
-        Assert.assertEquals(list.size(),1);
-        list.get(0).setForeignWord("Hello world");
-        tx.commit();
-        }catch (Exception e) {
-            e.printStackTrace();
-        }
-        finally {
-            //HibernateConfig.rollback();
-
-        }
-
-    }
+//        }catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//        finally {
+//            //HibernateConfig.rollback();
+//
+//        }
+//
+//    }
 
 }
